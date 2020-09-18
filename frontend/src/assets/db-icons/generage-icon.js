667631e400377dd06555235f9c09c093801d7c37
canwdev <c2097@live.com>
2020-09-18 19:10:54 +0800

    original icon

--- frontend/src/assets/db-icons/generage-icon.js
+++ frontend/src/assets/db-icons/generage-icon.js
@@ -2,7 +2,7 @@
 // https://github.com/keepassxreboot/keepassxc/tree/develop/share/icons/database
 const fs = require('fs')
 const path = require('path')
-fs.readdir(path.join(__dirname, 'icons'), {}, function (err,data) {
+fs.readdir(path.join(__dirname, 'icons2'), {}, function (err,data) {
   if (err) {
     console.error(err)
     return
--- frontend/src/assets/db-icons/index.js
+++ frontend/src/assets/db-icons/index.js
@@ -2,7 +2,7 @@ const iconMap = require('./icon-map.json')
 
 function Icons() {
   // 让 Webpack 自动打包图片
-  this.items = iconMap.map(item => require('./icons/' + item))
+  this.items = iconMap.map(item => require('./icons2/' + item))
 
   this.getByIndex = function (index) {
     return this.items[index]
